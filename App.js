e30cc854c0688f30a1ff9f9c3e9f82033bee6127
prasad5235692 <148196914+prasad5235692@users.noreply.github.com>
2025-06-20 21:07:34 +0530

    on change

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App1 from "./port/App1";
+function App() {
+  return (
+    <div className="app">
+      <App1/>
+    
+    </div>
+  );
+}
+
+export default App;
+
